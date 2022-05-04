654175494d5cb302af44d052869d7d7b9c40041e
ruanmoreira <ruaanmoreira2508@gmail.com>
2022-05-04 12:33:39 -0300

    Estrutura Criada

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+app.get ("/github", (request, response) => {
+    response.redirect (
+        `https://github.com/login/oauth/authorize?client_id=${process.env
+        .GITHUB_CLIENT_ID}`
+    )
+})
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+})
+app.listen (4000, () => console.log ('Serven is running on port 4000'));
